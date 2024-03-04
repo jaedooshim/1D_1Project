effe2980190ceeef3d705d789338389a1ea38b14
shimjaedoo <woen1637@naver.com>
2024-03-04 18:20:49 +0900

    feat : Auth Module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtModule } from '../_common/jwt/jwt.module';
+import { BcryptModule } from '../_common/bcrypt/bcrypt.module';
+import { MemberModule } from '../member/member.module';
 
-@Module({})
+@Module({
+  imports: [JwtModule, BcryptModule, MemberModule],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '../_common/jwt/jwt.service';
+import { BcryptService } from '../_common/bcrypt/bcrypt.service';
+import { MemberService } from '../member/member.service';
+import { ILogin } from './types/request.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    private bcryptService: BcryptService,
+    private memberService: MemberService,
+  ) {}
+
+  async login(data: ILogin): Promise<string> {
+    const member = await this.memberService.getEmail(data.email);
+    const isvalidPassword = await this.bcryptService.compare(data.password, member.password);
+    if (!isvalidPassword) throw new BadRequestException('비밀번호가 일치하지 않습니다.');
+
+    const payload = { id: member.id, email: member.email, name: member.name };
+    const accessToken = this.jwtService.sign(payload);
+    return accessToken;
+  }
+}
